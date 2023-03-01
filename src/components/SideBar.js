145e34d196f7101532ffd7cccc24565fc6c2e378
neadianyanwu <neadianyanwu@gmail.com>
2023-02-28 22:59:36 -1100

    Updated Sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,19 +1,41 @@
 import "./SideBar.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SunDay } from "react-basicons";
+import { MoonNight } from "react-basicons";
 
 const SideBar = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect (() => {
+    const body =document.body;
+    if (darkMode) {
+      body.classList.add("dark-mode");
+    } else {
+      body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  
   
   return (
     <div className="left" id="section">
         <h4 className="col">Customize</h4>
-    
-      <div className="reset">
-        <button>
-          Reset
-        </button>
-      </div>
 
+        <div id="button1">
+          <button onClick={handleDarkModeToggle}>
+            {darkMode ? <SunDay /> : <MoonNight />}
+          </button>
+        </div>
+
+        <div>
+          <button id="btn1">Reset</button>
+        </div>
     </div>
     
   )
